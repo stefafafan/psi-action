29b5aac7383cab4020659ea397ece729712a2e38
Stefan Utamaru <mail@stenyan.jp>
2022-12-21 22:47:34 +0900

    fix linter error

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,7 +1,4 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import {wait} from './wait'
 import {expect, test} from '@jest/globals'
 
 test('throws invalid number', async () => {
@@ -13,17 +10,6 @@ test('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
   const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
+  const delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
 })
